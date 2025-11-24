0fd5a17f6fdfe9d704c2d2da895b0d4cc3d2de12
dy_lim <ldy6740@gmail.com>
2025-11-24 15:33:46 +0900

    js 에서 타입을 체크하는 방법, 타입스크립트를 사용하면 좋은 점

---
+++ why-ts/sample.js
@@ -0,0 +1,14 @@
+// @ts-check를 입력하면 js에서 ts 처럼 타입을 체크 가능하다. 
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
+function add(a, b) {
+	return a + b
+}
+
+add(20,20);
---
+++ why-ts/sample.ts
@@ -0,0 +1,11 @@
+function sum(a: number, b: number): number {
+	return a + b;
+}
+
+var result = sum(20, 20);
+
+
+result.toLocaleString();
+
+// vscode IntelliSense => vscode 자동완성 기을을 타입을 설정함으로 설정한 타입의 API 를 VScode IntelliSense가 추론 하여 보여 준다.
+
